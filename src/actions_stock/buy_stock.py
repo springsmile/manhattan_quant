8fe90debbb0788d131ff9ca1eb0aebc5d4cc3345
tangkangqi <darcy_tang@foxmail.com>
2020-09-23 19:46:27 +0800

    更新买入类

--- src/actions_stock/buy_stock.py
+++ src/actions_stock/buy_stock.py
@@ -1,3 +1,5 @@
+import sys
+sys.path.append("..")
 import analys_stock
 import config as cfg
 import pandas as pd
@@ -5,16 +7,11 @@ import matplotlib.pyplot as plt
 AST = analys_stock.AnaStock(cfg=cfg)
 
 class BuyStock:
-    def __init__(self, cfg):
-        self.cfg = cfg
-
-    def buy(self, stock_price, before_cash_amount,
+    def __init__(self, stock_price, before_cash_amount,
             before_stock_num, dif_price = 0):
+
         # 实时股价
         self.stock_price = stock_price
-
-        # 报买价
-        self.buy_price = self.stock_price - dif_price
 
         # 买前现金
         self.before_cash_amount = before_cash_amount
@@ -22,13 +19,33 @@ class BuyStock:
         # 买前股数
         self.before_stock_num = before_stock_num
 
-        # 买入股数
+        # 报买差价
+        self.dif_price = dif_price
+
+    def quotes_price(self):
+        # 报买价 = 实时股价 - 报买差价
+        self.quotes_price = self.stock_price + self.dif_price
+        return self.quotes_price
+
+    def buy_price(self):
+        # 实际买价 = 报买价
+        self.buy_price = self.quotes_price
+
+        return self.quotes_price
+
+
+    def prepare_buy(self):
+        # 报买
+        self.quotes_price()
+        self.buy_price()
+
+        # 预计买入股数
         self.buy_stock_num = int(self.before_cash_amount / (100 * self.buy_price))*100
 
-        # 买股现金
+        # 预计买股现金
         self.buy_cash_amount = self.buy_stock_num * self.buy_price
 
-        # 佣金
+        # 预计佣金
         # 买入: 买股现金数 * 0.0006
         self.buy_commission = self.buy_cash_amount * 0.0006
 
@@ -37,17 +54,27 @@ class BuyStock:
 
 
         # 买后现金 = 买前现金 - 买股现金 - 佣金
+        # 预买后现金
+        self.after_cash_amount = self.before_cash_amount - self.buy_cash_amount - self.buy_commission
+
         # 注: 若买后现金数 < 0 ，
         # /方案1：则账户补充 =  -买后现金数， 买后现金数 = 0
         # /【未启用】方案2：无账户补充， 更新买入股数少持一手， 然后更新买股现金、 佣金、买后现金
 
-        self.after_cash_amount = self.before_cash_amount - self.buy_cash_amount - self.buy_commission
         if self.after_cash_amount < 0:
             self.account_add_cash = - self.after_cash_amount
             self.after_cash_amount = 0
 
         # 买后股数
         self.after_stock_num = self.before_stock_num + self.buy_stock_num
+
+        # 买后总资产
+        self.after_assets = self.stock_price * self.after_stock_num + self.after_cash_amount
+
+
+    def get_assets(self):
+        # 总资产
+        self.assets = self.stock_price * self.stock_num + self.cash_amount
 
 
     def strategy(self):
@@ -61,9 +88,9 @@ class BuyStock:
         principal = 100000
 
 
-
-
 if __name__ == '__main__':
-    SS = StrategicStock(cfg)
-    SS.load_data()
-    SS.strategy()
+    SS = BuyStock(stock_price=6, before_cash_amount=100000, before_stock_num=1000, dif_price=0.1)
+    SS.prepare_buy()
+    # SS.after_assets()
+    print(SS.stock_price, SS.buy_price)
+    print(SS.account_add_cash, SS.buy_commission, SS.after_cash_amount, SS.after_stock_num, SS.after_assets)
