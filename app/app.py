19495ef2e917cda3bf5e8f59b88c26fc56582831
tangkangqi <darcy_tang@foxmail.com>
2020-09-23 19:44:47 +0800

    update

--- app/app.py
+++ app/app.py
@@ -1,6 +1,12 @@
 import src
-from src.get_stock import func
+from src import config
+from src import get_stock as gs
+# import src.get_stock as gs
+# from gs import func
 
 if __name__ == '__main__':
-    func()
+    # func()
+    gs.fun()
+    src.config
+    # s
     pass
