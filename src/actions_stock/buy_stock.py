3911e2f9f3a2f3b2c101cb2d1e5f1d6ab7fe00ac
tangkangqi <darcy_tang@foxmail.com>
2020-09-29 00:05:39 +0800

    update

--- src/actions_stock/buy_stock.py
+++ src/actions_stock/buy_stock.py
@@ -5,6 +5,76 @@ import config as cfg
 import pandas as pd
 import matplotlib.pyplot as plt
 AST = analys_stock.AnaStock(cfg=cfg)
+from stock import *
+
+class BuyStock2:
+    def __init__(self, stock, account, cfg):
+
+        self.stock = stock
+
+        self.account = account
+        # 实时股价
+        self.stock_price = stock.stock_price
+
+        # 买前现金
+        self.before_cash_amount = account.cash_amount
+
+        # 买前股数
+        self.before_stock_num = stock.stock_num
+
+        # 报买差价
+        self.dif_price = cfg.dif_price
+
+    def quotes_price(self):
+        # 报买价 = 实时股价 - 报买差价
+        self.quotes_price = self.stock_price + self.dif_price
+        return self.quotes_price
+
+    def buy_price(self):
+        # 实际买价 = 报买价
+        self.buy_price = self.quotes_price
+
+        return self.quotes_price
+
+    def prepare_buy(self):
+        # 报买
+        self.quotes_price()
+        self.buy_price()
+
+        # 预计买入股数
+        self.buy_stock_num = int(self.before_cash_amount / (100 * self.buy_price))*100
+
+        # 预计买股现金
+        self.buy_cash_amount = self.buy_stock_num * self.buy_price
+
+        # 预计佣金
+        # 买入: 买股现金数 * 0.0006
+        self.buy_commission = self.buy_cash_amount * 0.0006
+
+        # 账户补充
+        self.account_add_cash = 0
+
+
+        # 买后现金 = 买前现金 - 买股现金 - 佣金
+        # 预买后现金
+        self.after_cash_amount = self.before_cash_amount - self.buy_cash_amount - self.buy_commission
+
+        # 注: 若买后现金数 < 0 ，
+        # /方案1：则账户补充 =  -买后现金数， 买后现金数 = 0
+        # /【未启用】方案2：无账户补充， 更新买入股数少持一手， 然后更新买股现金、 佣金、买后现金
+
+        if self.after_cash_amount < 0:
+            self.account_add_cash = - self.after_cash_amount
+            self.after_cash_amount = 0
+
+        # 买后股数
+        self.after_stock_num = self.before_stock_num + self.buy_stock_num
+
+        # 买后总资产
+        self.after_assets = self.stock_price * self.after_stock_num + self.after_cash_amount
+
+
+
 
 class BuyStock:
     def __init__(self, stock_price, before_cash_amount,
